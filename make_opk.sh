b22071cd4a8fcc7556ae30fa7a7fc5ce099a52e2
gameblabla <gameblabla@openmailbox.org>
2020-01-11 20:58:40 +0100

    Minor changes and rebuilding with older toolchain. Seems to fix 30 FPS issue.

--- make_opk.sh
+++ make_opk.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-OPK_NAME=pcsx4all.opk
+OPK_NAME=pcsx4all_"$1".opk
 
 echo ${OPK_NAME}
 
 # create default.gcw0.desktop
-cat > default.gcw0.desktop <<EOF
+cat > default."$1".desktop <<EOF
 [Desktop Entry]
 Name=Pcsx4all
 Comment=Sony PSX emulator
@@ -20,11 +20,11 @@ EOF
 
 # create opk
 FLIST="pcsx4all"
-FLIST="${FLIST} default.gcw0.desktop"
+FLIST="${FLIST} default.$1.desktop"
 FLIST="${FLIST} src/port/sdl/pcsxr-icon-small.png"
 
 rm -f ${OPK_NAME}
 mksquashfs ${FLIST} ${OPK_NAME} -all-root -no-xattrs -noappend -no-exports
 
-cat default.gcw0.desktop
-rm -f default.gcw0.desktop
+cat default."$1".desktop
+rm -f default."$1".desktop
